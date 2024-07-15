af3cce2a7db894be321a1fb393f62eed51c93e79
mannk98 <khacman98@gmail.com>
2024-07-15 14:54:14 +0700

    update

---
+++ cmd/root.go
@@ -0,0 +1,185 @@
+/*
+Copyright © 2024 mannk khacman98@gmail.com
+*/
+package cmd
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/mannk98/goutils/sqlutils"
+	"github.com/mannk98/goutils/utils"
+	log "github.com/sirupsen/logrus"
+	"github.com/sonnt85/gosutils/sched"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile        string
+	Logger         = log.New()
+	LogLevel       = log.ErrorLevel
+	LogFile        = "cloudstackSetupHost.log"
+	cfgFileDefault = ".cloudstackSetupHost"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "cloudstackSetupHost",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: rootRun,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	utils.InitLogger(LogFile, Logger, LogLevel)
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cloudstackSetupHost.toml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		if err != nil {
+			Logger.Error(err)
+			os.Exit(1)
+		}
+
+		cfgFile = cfgFileDefault
+		// Search config in home directory with name ".cloudstackSetupHost" (without extension).
+		viper.AddConfigPath(home)
+		viper.AddConfigPath("./")
+		viper.SetConfigType("toml")
+		viper.SetConfigName(cfgFile)
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Error("config.toml file at ./ folder is not exist. Create it first.")
+		} else {
+			Logger.Error(err)
+		}
+	} else {
+		Logger.Info("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func rootRun(cmd *cobra.Command, args []string) {
+	mysqlUser := viper.GetString("mysqlUser")
+	mysqlPassword := viper.GetString("mysqlPassword")
+	mysqlHost := viper.GetString("mysqlHost")
+	defaultHostName := viper.GetString("defaultHostName")
+	runInterval := viper.GetInt("runInterval")
+
+	var mysqlconnection *sql.DB
+	var err error
+	for {
+		mysqlCfg := sqlutils.MysqlInitConfig(mysqlUser, mysqlPassword, mysqlHost, "3306", "cloud")
+		mysqlconnection, err = sqlutils.MysqlPing(mysqlCfg)
+		if err != nil {
+			Logger.Errorf("can't connect to Mysqldb at %s: %v", err, mysqlHost)
+			time.Sleep(5 * time.Second)
+		} else {
+			break
+		}
+	}
+
+	_, err = MysqlExec(mysqlconnection, "update configuration set value = 'false'  where name = 'ca.plugin.root.auth.strictness';")
+	if err != nil {
+		Logger.Error("Failed update ca.plugin.root.auth.strictness to false, error: ", err)
+	}
+
+	job := func(sched *sched.Job) {
+		mysqlCfg := sqlutils.MysqlInitConfig(mysqlUser, mysqlPassword, mysqlHost, "3306", "cloud")
+		mysqlconnection, err := sqlutils.MysqlPing(mysqlCfg)
+		if err != nil {
+			Logger.Errorf("can't connect to Mysqldb at %s: %v", err, mysqlHost)
+		} else {
+			queryRam := fmt.Sprintf("select ram from host where name = '%s';", defaultHostName)
+			queryCpu := fmt.Sprintf("select cpus from host where name = '%s';", defaultHostName)
+
+			var realCpus, realRam int
+			err := mysqlconnection.QueryRow(queryCpu).Scan(&realCpus)
+			if err != nil {
+				Logger.Error(err)
+			}
+			err = mysqlconnection.QueryRow(queryRam).Scan(&realRam)
+			if err != nil {
+				Logger.Error(err)
+			}
+
+			// when new host exist
+			if realCpus != 0 && realRam != 0 {
+				// set new variable
+				host_uuid, _ := uuid.NewRandom()
+				host_guid := host_uuid.String() + "-LibvirtComputingResource"
+
+				updateUUID := fmt.Sprintf("update host set uuid = '%s', guid = '%s', ram = %d, cpus = %d where name = 'cloudstack-agentkvm';", host_uuid.String(), host_guid, realRam*3, realCpus*3)
+				updateHostName := fmt.Sprintf(" update host set name = 'newHost-%s' where uuid = '%s';", host_uuid.String(), host_uuid.String())
+
+				_, err := MysqlExec(mysqlconnection, updateUUID)
+				if err != nil {
+					Logger.Errorf("failed update Uuid using command: %s, Error: %v", updateUUID, err)
+				}
+				_, err = MysqlExec(mysqlconnection, updateHostName)
+				if err != nil {
+					Logger.Errorf("failed update Uuid using command: %s, Error: %v", updateHostName, err)
+				}
+			}
+		}
+
+	}
+	sched.Every(runInterval).ESeconds().Run(job)
+	// Keep the program from not exiting.
+	defer mysqlconnection.Close()
+	runtime.Goexit()
+}
+
+func MysqlExec(database *sql.DB, query string) (sql.Result, error) {
+	//doc_type := 0
+	//query := fmt.Sprintf("UPDATE history SET request_datetime = DATE_ADD(request_datetime, INTERVAL 7 HOUR) WHERE doc_type=%d AND request_datetime BETWEEN '2024-02-01 00:00:00' AND '2024-02-20 23:59:59'", doc_type)
+
+	//fmt.Println(query)
+	Result, err := database.Exec(query)
+	if err != nil {
+		return nil, err
+	}
+	return Result, err
+}
---
+++ go.mod
@@ -0,0 +1,51 @@
+module cloudstackSetupHost
+
+go 1.21.4
+
+require (
+	github.com/mannk98/goutils/utils v0.0.0-20240606080256-7f5de4ee8507
+	github.com/sirupsen/logrus v1.9.3
+	github.com/sonnt85/gosutils/sched v0.0.0-20230927031609-2b3046a0b311
+	github.com/spf13/cobra v1.8.1
+	github.com/spf13/viper v1.19.0
+)
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/antchfx/jsonquery v1.3.4 // indirect
+	github.com/antchfx/xpath v1.3.0 // indirect
+	github.com/basgys/goxml2json v1.1.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mannk98/goutils/sqlutils v0.0.0-20240606080256-7f5de4ee8507 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	github.com/sagikazarmark/locafero v0.4.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sonnt85/gosutils/endec v0.0.0-20230927031609-2b3046a0b311 // indirect
+	github.com/sonnt85/gotimeutils v0.0.0-20221011032526-b825ab5ef455 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.11.0 // indirect
+	github.com/spf13/cast v1.6.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tidwall/gjson v1.17.1 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
